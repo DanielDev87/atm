5c43b8da0563a0d44d7640685014645a50a2fd7d
DanielDev87 <danielhymtj@icloud.com>
2025-05-05 08:58:25 -0500

    Se ctea los servicios del movimiento entity

---
+++ src/main/java/com/bangroseron/atm/services/RetiroService.java
@@ -0,0 +1,10 @@
+package com.bangroseron.atm.services;
+
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class RetiroService {
+
+}
