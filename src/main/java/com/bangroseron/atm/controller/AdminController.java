824f6bad56baf4eeb224bf866038c32988a7d225
DanielDev87 <danielhymtj@icloud.com>
2025-05-07 08:54:59 -0500

    ceando el index de admin

--- src/main/java/com/bangroseron/atm/controller/AdminController.java
+++ src/main/java/com/bangroseron/atm/controller/AdminController.java
@@ -3,11 +3,26 @@ package com.bangroseron.atm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bangroseron.atm.services.ClienteService;
+import com.bangroseron.atm.services.CuentaService;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
+
+    private final ClienteService clienteService;
+    private final CuentaService cuentaService;
+
+    @GetMapping
+    public String adminHome(){
+        return "admin/index";
+    }
+    
 
 }
