bdc0ca8f3d4773f153925d5405a96d1de5c3234b
DanielDev87 <danielhymtj@icloud.com>
2025-05-14 08:17:25 -0500

    Metodos para crear-cliente

--- src/main/java/com/bangroseron/atm/controller/AdminController.java
+++ src/main/java/com/bangroseron/atm/controller/AdminController.java
@@ -1,14 +1,20 @@
 package com.bangroseron.atm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bangroseron.atm.entity.Cliente;
 import com.bangroseron.atm.services.ClienteService;
 import com.bangroseron.atm.services.CuentaService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -23,6 +29,20 @@ public class AdminController {
     public String adminHome(){
         return "admin/index";
     }
+
+    @GetMapping("/crear-cliente")
+    public String mostrarFormularioCliente(Model model) {
+        model.addAttribute("cliente", new Cliente());
+        return "admin/crear-cliente";
+    }
+
+    @PostMapping("/crear-cliente")
+    public String crearCliente(@ModelAttribute Cliente cliente) {
+        clienteService.crearCliente(cliente);      
+        return "redirect:/admin";
+    }
+    
+    
     
 
 }
