f0a8e43bd78b5926682e855157f08f1ed63ab9fe
DanielDev87 <danielhymtj@icloud.com>
2025-05-19 08:19:48 -0500

    completando el controlador admin

--- src/main/java/com/bangroseron/atm/controller/AdminController.java
+++ src/main/java/com/bangroseron/atm/controller/AdminController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bangroseron.atm.entity.Cliente;
+import com.bangroseron.atm.entity.Cuenta;
+import com.bangroseron.atm.entity.TipoCuenta;
 import com.bangroseron.atm.services.ClienteService;
 import com.bangroseron.atm.services.CuentaService;
 
@@ -41,6 +43,40 @@ public class AdminController {
         clienteService.crearCliente(cliente);      
         return "redirect:/admin";
     }
+
+    @GetMapping("/crear-cuenta")
+    public String mostrarFormularioCuenta(Model model) {
+        model.addAttribute("cuenta", new Cuenta());     
+        return "admin/crear-cuenta";
+    }
+
+    @PostMapping("/crear-cuenta")
+    public String crearCuenta(@RequestParam String identificacion,
+                            @RequestParam String numero,
+                            @RequestParam TipoCuenta tipo,
+                            @RequestParam double saldo) {
+        Cliente cliente = clienteService.buscarPorIdentificacion
+        (identificacion).orElseThrow();
+        cuentaService.crearCuenta(cliente, numero, tipo, saldo);       
+        
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/desbloquear")
+    public String mostrarDesbloqueo() {
+        return "admin/desbloquear";
+    }
+
+    @PostMapping("/desbloquear")
+    public String desbloquearCuenta(@RequestParam String identificacion,
+    @RequestParam String nuevoPin) {
+        clienteService.desbloquearCliente(identificacion, nuevoPin);
+        return "redirect:/admin";
+    }
+    
+    
+
+
     
     
     
