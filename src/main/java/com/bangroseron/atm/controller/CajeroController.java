3504b62dfdf8686a171051d4e1aef46ada1cc65f
DanielDev87 <danielhymtj@icloud.com>
2025-05-26 08:55:11 -0500

    metodo titular

--- src/main/java/com/bangroseron/atm/controller/CajeroController.java
+++ src/main/java/com/bangroseron/atm/controller/CajeroController.java
@@ -1,5 +1,7 @@
 package com.bangroseron.atm.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bangroseron.atm.dto.TransferenciaForm;
@@ -188,6 +191,14 @@ public class CajeroController {
     }
 
     return "cajero/transferir";
+    }
+
+    @GetMapping("/titular")
+    @ResponseBody
+    public Map<String, String> obtenerTitular(@RequestParam String numero){
+        return  cuentaService.buscarPorNumero(numero)
+        .map(cuenta -> Map.of("nombre",cuenta.getCliente().getNombreCompleto()))
+        .orElse(Map.of());   
     }
 
 }
